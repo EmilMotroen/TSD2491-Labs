ddb86a3fef1c5505fa061b00529d86bafa69fa1f
EmilMotroen <e.motroen@gmail.com>
2024-03-07 23:56:10 +0100

    Add project files.

---
+++ TSD2491-Labs/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TSD2491_Labs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Right click on the project in the solution explorer and select 'Set as Startup Project',",
+                " or use the drop down menu next to the green arrow, ",
+                " then that project can be run with the green arrow.");
+        }
+    }
+}
